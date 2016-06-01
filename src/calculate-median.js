60bee82a3b2fa272340bcd5b039475bd23ad2bd2
Aym-fuhong <18829287015@163.com>
2016-06-01 21:30:37 +0800

    修改

--- src/calculate-median.js
+++ src/calculate-median.js
@@ -1,10 +1,16 @@
 function calculate_median(arr) {
-  var num;
-    var arr1 = arr.sort(compare);
-    if(arr1.length%2 == 1){
-    	num = arr1[parseInt(arr1.length/2)];
+   var num;
+   var arr1 = [];
+   for (var i = 0;i < arr.length; i++) {
+   	 if (i%2 == 1) {
+   	 	arr1.push(arr[i]);
+   	 }  
+   }
+    var arr2 = arr1.sort(compare);
+    if(arr2.length%2 == 1){
+    	num = arr2[parseInt(arr2.length/2)];
     }else{
-    	num = (arr1[parseInt(arr1.length/2-1)]+arr1[parseInt(arr1.length/2)])/2;
+    	num = (arr2[parseInt(arr2.length/2-1)]+arr2[parseInt(arr2.length/2)])/2;
     }
     return num;
 }
